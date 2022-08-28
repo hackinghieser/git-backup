7cc75bccf8d678b13b8eb7450f4a1f56428d0654
Alexander Hieser <alex@macbook-pro.speedport.ip>
2022-08-28 22:43:30 +0200

    Fix pylint issuse and merge with main

--- git_handler.py
+++ git_handler.py
@@ -1,21 +1,28 @@
+"""
+Handles Git functions
+"""
 from asyncio.subprocess import STDOUT
 import subprocess
 from progress.bar import Bar
 
-"""
-Implements different methods
-to handle git repositories
-"""
-class git_handler:
+class GitHandler:
     """
-    Clone provided repository into
-    destination folder
-    user full_name for directory name
+    Implements different methods
+    to handle git repositories
     """
-    def cloneRepo(self,repo:any,token:str,mirror:bool,bar:Bar,destination:str):
+    def clone_repo(self,repo:any,token:str,mirror:bool,progress_bar:Bar,destination:str):
+        """
+        Clone provided repository into
+        destination folder
+        user full_name for directory name
+        """
         repo_fullname = repo['full_name']
-        bar.message = f"Clone:{repo_fullname}"
+        progress_bar.message = f"Clone:{repo_fullname}"
         if mirror:
-            subprocess.call(["git", "clone", "--mirror", f"https://{token}@github.com/"+ repo_fullname +".git",f"{destination}/{repo_fullname}", "--verbose"], stdout=subprocess.DEVNULL, stderr=STDOUT)
+            subprocess.call(["git", "clone", "--mirror",
+                             f"https://{token}@github.com/ {repo_fullname}.git",f"{destination}/{repo_fullname}",
+                              "--verbose"], stdout=subprocess.DEVNULL, stderr=STDOUT)
         else:
-            subprocess.call(["git", "clone", f"https://{token}@github.com/"+ repo_fullname +".git",f"{destination}/{repo_fullname}", "--verbose"], stdout=subprocess.DEVNULL, stderr=STDOUT)
+            subprocess.call(["git", "clone", 
+                            f"https://{token}@github.com/ {repo_fullname}.git",f"{destination}/{repo_fullname}",
+                             "--verbose"], stdout=subprocess.DEVNULL, stderr=STDOUT)
