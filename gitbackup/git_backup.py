52016f50e22d3d3619901c4267db81f3bcabf7f7
Alexander Hieser <Alexander.Hieser@online.de>
2022-09-04 18:49:02 +0200

    Check if git is installed before running backup

--- gitbackup/git_backup.py
+++ gitbackup/git_backup.py
@@ -3,10 +3,15 @@ GitBackup script
 """
 import argparse
 import json
+from pickle import TRUE
 import requests
 from progress.bar import Bar
 from gitbackup.githandler import GitHandler
+from shutil import which
 
+def check_git():
+    """Check if 'GIT' is in path"""
+    return which('git') is not None;
 
 def get_repositoires(token=str):
     """
@@ -29,7 +34,11 @@ def get_repositoires(token=str):
     return repos
 
 
-def clone_repositories(git=GitHandler, destination=str, username=str, access_token=str, mirror=bool):
+def clone_repositories(git=GitHandler,
+                       destination=str,
+                       username=str,
+                       access_token=str,
+                       mirror=bool):
     """
     Clone repositories from user
     into the destination path
@@ -46,17 +55,17 @@ def clone_repositories(git=GitHandler, destination=str, username=str, access_tok
             if username != "":
                 if repo["owner"]["login"] == username:
                     git.clone_repo(repo=repo,
-                                        token=access_token,
-                                        mirror=mirror,
-                                        progress_bar=progress_bar,
-                                        destination=destination)
+                                   token=access_token,
+                                   mirror=mirror,
+                                   progress_bar=progress_bar,
+                                   destination=destination)
                     progress_bar.next()
             else:
                 git.clone_repo(repo=repo,
-                                    token=access_token,
-                                    mirror=mirror,
-                                    progress_bar=progress_bar,
-                                    destination=destination)
+                               token=access_token,
+                               mirror=mirror,
+                               progress_bar=progress_bar,
+                               destination=destination)
                 progress_bar.next()
 
 
@@ -68,23 +77,27 @@ def run_cli():
           ":!!   !!: !!:   !!:   !!:  !!! !!:  !!! :!!      !!: :!!  !!:  !!! !!: \n" +
           " :: :: :  :      :    :: : ::   :   : :  :: :: :  :   :::  :.:: :   :     \n \n ")
 
-    # Setup CLI arguments
-
-    parser = argparse.ArgumentParser(description='Welcome, use cmd line arguments ' +
-                                     'or wizard to create your remote backups.')
-    parser.add_argument('-t', type=str, required=True, dest="access_token",
-                        help='Your Github API Access Token')
-    parser.add_argument('-p', type=str, dest="destination",
-                        help='Backup destination path')
-    parser.add_argument('-u', type=str, dest="username",
-                        help='Username')
-    parser.add_argument('--mirror', action="store_true",
-                        dest="mirror", help="Git Mirror repositories")
-    args = parser.parse_args()
-    access_token = args.access_token or ""
-    destination = args.destination or "."
-    username = args.username or ""
-    mirror = args.mirror
-
-    git = GitHandler()
-    clone_repositories(git=git,username=username,destination=destination,mirror=mirror,access_token=access_token)
+    # Check if git is installed
+    if check_git() is True:
+        # Setup CLI arguments
+        parser = argparse.ArgumentParser(description='Welcome, use cmd line arguments ' +
+                                        'or wizard to create your remote backups.')
+        parser.add_argument('-t', type=str, required=True, dest="access_token",
+                            help='Your Github API Access Token')
+        parser.add_argument('-p', type=str, dest="destination",
+                            help='Backup destination path')
+        parser.add_argument('-u', type=str, dest="username",
+                            help='Username')
+        parser.add_argument('--mirror', action="store_true",
+                            dest="mirror", help="Git Mirror repositories")
+        args = parser.parse_args()
+        access_token = args.access_token or ""
+        destination = args.destination or "."
+        username = args.username or ""
+        mirror = args.mirror
+        git = GitHandler()
+        clone_repositories(git=git, username=username, destination=destination,
+                        mirror=mirror, access_token=access_token)
+    else:
+        print('GIT not found in path. Please install git and retry.')
+        exit()
